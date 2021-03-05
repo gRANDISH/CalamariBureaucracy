8850ad003a95f820a992d448a8e239427513c89e
gRANDISH <30308620+gRANDISH@users.noreply.github.com>
2021-03-04 23:43:29 -0600

    moved again
    maybe...correct...this time?

---
+++ CalamariBureaucracy/Intermediate/Build/Win64/UE4Editor/Inc/CalamariBureaucracy/CalamariBureaucracyGameMode.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CALAMARIBUREAUCRACY_CalamariBureaucracyGameMode_generated_h
+#error "CalamariBureaucracyGameMode.generated.h already included, missing '#pragma once' in CalamariBureaucracyGameMode.h"
+#endif
+#define CALAMARIBUREAUCRACY_CalamariBureaucracyGameMode_generated_h
+
+#define CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_SPARSE_DATA
+#define CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_RPC_WRAPPERS
+#define CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACalamariBureaucracyGameMode(); \
+	friend struct Z_Construct_UClass_ACalamariBureaucracyGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ACalamariBureaucracyGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/CalamariBureaucracy"), CALAMARIBUREAUCRACY_API) \
+	DECLARE_SERIALIZER(ACalamariBureaucracyGameMode)
+
+
+#define CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesACalamariBureaucracyGameMode(); \
+	friend struct Z_Construct_UClass_ACalamariBureaucracyGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ACalamariBureaucracyGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/CalamariBureaucracy"), CALAMARIBUREAUCRACY_API) \
+	DECLARE_SERIALIZER(ACalamariBureaucracyGameMode)
+
+
+#define CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	CALAMARIBUREAUCRACY_API ACalamariBureaucracyGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACalamariBureaucracyGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(CALAMARIBUREAUCRACY_API, ACalamariBureaucracyGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACalamariBureaucracyGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	CALAMARIBUREAUCRACY_API ACalamariBureaucracyGameMode(ACalamariBureaucracyGameMode&&); \
+	CALAMARIBUREAUCRACY_API ACalamariBureaucracyGameMode(const ACalamariBureaucracyGameMode&); \
+public:
+
+
+#define CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	CALAMARIBUREAUCRACY_API ACalamariBureaucracyGameMode(ACalamariBureaucracyGameMode&&); \
+	CALAMARIBUREAUCRACY_API ACalamariBureaucracyGameMode(const ACalamariBureaucracyGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(CALAMARIBUREAUCRACY_API, ACalamariBureaucracyGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACalamariBureaucracyGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ACalamariBureaucracyGameMode)
+
+
+#define CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_9_PROLOG
+#define CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_SPARSE_DATA \
+	CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_RPC_WRAPPERS \
+	CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_INCLASS \
+	CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_SPARSE_DATA \
+	CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CALAMARIBUREAUCRACY_API UClass* StaticClass<class ACalamariBureaucracyGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID CalamariBureaucracy_Source_CalamariBureaucracy_CalamariBureaucracyGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Source/CalamariBureaucracy/CalamariBureaucracy.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CalamariBureaucracy.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CalamariBureaucracy, "CalamariBureaucracy" );
-
-DEFINE_LOG_CATEGORY(LogCalamariBureaucracy)
- 
